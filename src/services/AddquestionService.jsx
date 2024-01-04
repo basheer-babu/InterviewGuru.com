1f416080458d23dd0839cf7b7c878e3384bdd953
ShreyaSuddala27 <144776459+ShreyaSuddala27@users.noreply.github.com>
2024-01-04 14:19:48 +0530

    Delete src directory

--- src/services/AddquestionService.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-export const AddQuestionservice ={
-    createquestion: async function(){
-        return await axios.post(`https://interviewguru.onrender.com/api/user/save`).then((res)=>res.data)
-    }
-}
-// formData.questions.split(',').map((question) => question.trim());
--- src/services/fetchAlldetails.jsx
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const fetchAll = async ()=>{
-    return await axios.get(`https://interviewguru.onrender.com/api/uuser/fetchAll`).then((res)=>res.data)
-}
-
